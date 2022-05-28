1902d3a72d8f661836f6284b3de31facb49f47ef
natybum <106363113+natybum@users.noreply.github.com>
2022-05-27 22:20:23 -0300

    Update scripto.js

--- primer tp/js/scripto.js
+++ primer tp/js/scripto.js
@@ -1,12 +1,5 @@
 console.log("Hola mundo!!");
 
-document.write( "");
-
-var numero = 123;
-
-console.log (numero)
-
-var nombreCompletoUsusario = "Juan Perez";
 
 console.log(nombreCompletoUsusario);
 
@@ -16,30 +9,7 @@ console.log("el IVA es ", IVA, "%")
 
 var num1 = {} ;
 
-console.log(typeof num1);
-
-const auto = {
-    ruedas: 4,
-    marca: "fiat",
-    modelo: 2021
-}; 
-console.log(auto, auto.marca);
-
-const garage = {
-    espacio_1: auto,
-}
-
-var num2 = "123"
-num2= Number(num2);
-console.log(typeof num2, num2, num2+1);
-
-var num3 = "123.23t";
-num3 = parseInt(num3);
-console.log(num3);
-
-var num4 = "123.23t";
-num4 = parseFloat(num4);
-console.log(num4);
+console.log(typeof number)
 
 var txt1= "juan";
 var txt2= "pablo";
